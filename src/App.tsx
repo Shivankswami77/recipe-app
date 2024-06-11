bf7aff5ff056cd1aa380044a3933814186da1b2b
Shivank Swami <69349438+Shivankswami77@users.noreply.github.com>
2024-06-11 17:33:52 +0530

    added lodash and search form in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
+import { useState } from "react";
 import "./App.css";
 import RecipeForm from "./components/RecipeForm";
 import RecipeList from "./components/RecipeList";
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
   return (
     <>
-      <RecipeList />
-      <RecipeForm />
+      <div>
+        <RecipeForm />
+        <input
+          type="text"
+          placeholder="Search recipes..."
+          value={searchTerm}
+          onChange={handleSearchChange}
+        />
+        <button>Sort by Time (Asc)</button>
+        <button>Sort by Time (Desc)</button>
+        <RecipeList />
+      </div>
     </>
   );
 }
