51aaa2b8a94dd2fd9701e5117b6f824724f40208
Shivank Swami <69349438+Shivankswami77@users.noreply.github.com>
2024-06-11 17:30:18 +0530

    Added components for Recipe Form and Recipe list into App.tsx

---
+++ src/components/RecipeForm.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RecipeForm: React.FC = () => {
+  return <div>This is recipe form</div>;
+};
+
+export default RecipeForm;
---
+++ src/components/RecipeList.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RecipeList: React.FC = () => {
+  return <div>This is recipe list page</div>;
+};
+
+export default RecipeList;
